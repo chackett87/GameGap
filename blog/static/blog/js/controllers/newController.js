44bc8e07781c0eb4869e0e80a09b7598ebb37c54
Chris Hackett <chackett87@gmail.com>
2015-05-31 13:46:22 -0700

    stuff

--- blog/static/blog/js/controllers/newController.js
+++ blog/static/blog/js/controllers/newController.js
@@ -2,11 +2,12 @@
  * Created by gkadillak1 on 5/20/15.
  */
 // We're referencing our declared variable 'app' here
-app.controller('newController', function($http) {
+app.controller('newController', function($http, $scope) {
     // Controller logic goes here
     console.log('Our controller is working');
     $http.get('/api/blog/entries')
         .then(function(data) {
             console.log(data);
         });
+  $scope.name='Chris'
 });
