38a9edbc253408647e6b9788a548f08f000278c5
Chris Hackett <chackett87@gmail.com>
2015-05-31 14:25:57 -0700

    added databases to settings.py

--- django_and_angular/settings.py
+++ django_and_angular/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'django_and_angular.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'drf_and_angular',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'drf_and_angular',
+        }
+    }
 
 
 # Internationalization
