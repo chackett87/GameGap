452c1f18840de06d9fa988aff57a5eb550ea1905
Chris Hackett <chackett87@gmail.com>
2015-06-21 22:02:47 -0700

    added hover features and removed Top_grossing section due to redundancy

--- gameGap/static/gameGap/js/controllers/topFreeController.js
+++ gameGap/static/gameGap/js/controllers/topFreeController.js
@@ -4,7 +4,7 @@ app.controller('topFreeController', function($http) {
   self.z = 'Stuff'; 
 
   self.games = []; 
-  var url = "https://42matters.com/api/1/apps/top_google_charts.json?callback=JSON_CALLBACK&list_name=topselling_free&cat_key=GAMES&country=US&limit=6&access_token=852c36f637f083e4e5a01abfc1a8ac384f694065";   
+  var url = "https://42matters.com/api/1/apps/top_google_charts.json?callback=JSON_CALLBACK&list_name=topselling_free&cat_key=GAMES&country=US&limit=3&access_token=852c36f637f083e4e5a01abfc1a8ac384f694065";   
   var responsePromise = $http.jsonp(url);
   responsePromise.success(function(data) {
     self.games = data.appList;
