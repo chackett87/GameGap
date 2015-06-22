9deda54d91c726a8c326b5b85cc31fe7c60cd617
Chris Hackett <chackett87@gmail.com>
2015-06-22 10:14:48 -0700

    updated some basic CSS

--- gameGap/api/serializers/entry_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from blog.models import Entry
-
-
-class EntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Entry
--- gameGap/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from blog.api.views.entry_view import EntryView
-
-urlpatterns = [
-    url(r'^entries/$', EntryView.as_view()),
-]
